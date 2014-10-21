2f3aa19c53ace4e84275ac8c308e4c932e2f9a14
Richard Heng <rheng001@well.cs.ucr.edu>
2014-10-21 14:38:06 -0700

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <boost/tokenizer.hpp> //token
+#include <unistd.h> //fork
+#include <sys/types.h> 
+#include <sys/wait.h> //wait pid
+#include <stdio.h> //perror
+
+#include <string>
+
 
 using namespace std;
+using namespace boost;
 
 int main()
 {
