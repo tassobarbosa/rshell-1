841b8160c3379c10cbb5a987c1f5dbf050c9cc2d
Richard Heng <rheng001@well.cs.ucr.edu>
2014-10-21 23:45:49 -0700

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
-#include <boost/tokenizer.hpp> //token
 #include <unistd.h> //fork
 #include <sys/types.h> 
 #include <sys/wait.h> //wait pid
 #include <stdio.h> //perror
 
 #include <string>
-
+#include <string.h>
 
 using namespace std;
-using namespace boost;
 
-int main()
+int main(int argc, char *argv)
 {
+
+	//char * cmd;
+	//char length[10];
 	
+	int pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Fork error");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		cout << "Child Process" << endl;
+		if (-1 == execvp(argv[0], argv))
+		{
+			perror("execvp error");
+		
+			exit(1);
+		}
+	}
+
+	else if (pid > 0)
+	{
+		if (-1 == wait(0))
+		{
+			cout << "I'm a parent." << endl;
+			perror("error with wait");
+		}
+	}
+
 	return 0;
+
+
+
+
 }
